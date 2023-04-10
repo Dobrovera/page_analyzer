306a2cdf8a142052d5a49bc02026311188a8e062
Vera Dobrodeeva <dobro.vera@gmail.com>
2023-04-10 11:33:21 +0300

    step 3

---
+++ database.sql
@@ -0,0 +1,4 @@
+CREATE TABLE urls (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    created_at date
+);
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,18 +1,16 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
-from flask import render_template
-
-
-app = Flask(__name__)
+from flask import Flask, render_template
 
 
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', False)
+DATABASE_URL = os.getenv('DATABASE_URL')
+app = Flask(__name__)
 
 
-@app.route('/')
+@app.get('/')
 def hello_world():
     return render_template(
         'index.html',
