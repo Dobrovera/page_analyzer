c8aee596bbbe8ef427b52b075d537efb0eac58ee
Vera Dobrodeeva <dobro.vera@gmail.com>
2023-05-06 20:52:55 +0300

    edits №5

--- page_analyzer/check_req.py
+++ page_analyzer/check_req.py
@@ -13,9 +13,11 @@ def check_request(curs, id):
         url_info['title'] = str(soup.find('title').text) \
             if soup.find('title') else ''
         url_info['h1'] = str(soup.find('h1').text) if soup.find('h1') else ''
-        url_info['description'] = str(soup.find_all
-                                      ('meta',
-                                       attrs={'name': 'description'}))[16:-23]
+        description = str(soup.find
+                          ('meta',
+                           attrs={'name': 'description'}))
+        url_info['description'] = description['content'].strip() \
+            if description else ''
 
         if 200 <= url_info['status_code'] <= 299:
             return url_info
